0bb5bdcabead462f5467854c07cc6fbeb37ea5a3
Anastasija Semen <anastasija.semen@gmail.com>
2018-09-03 16:59:42 +0300

    change function

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const a = prompt("Enter coefficient a");
-const b = prompt("Enter coefficient b");
-const c = prompt("Enter coefficient c");
+const a = parseFloat(prompt("Enter coefficient a"));
+const b = parseFloat(prompt("Enter coefficient b"));
+const c = parseFloat(prompt("Enter coefficient c"));
 
 const x = solveSquareEquation(a, b, c);
 
@@ -13,11 +13,13 @@ function solveSquareEquation(a, b, c) {
     if (d < 0) {
         return "There is no solution to the equation";
     } else {
-        const x1 = (-2 - Math.sqrt(d)) / (2 * a);
-        const x2 = (-2 + Math.sqrt(d)) / (2 * a);
+        const x1 = (-b - Math.sqrt(d)) / (2 * a);
+        const x2 = (-b + Math.sqrt(d)) / (2 * a);
         return "There is two solution to the equation: " + x1 + " and " + x2;
     }
 }
+
+
 
 
 
